de1658e6bbde375ad31fa2a051d316692a1d8000
Vedansh1506 <vedanshpatel1506@gmail.com>
2024-09-11 20:25:13 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,66 +1,73 @@
 //{ Driver Code Starts
-// C program for insertion sort
-#include <stdio.h>
-#include <math.h>
+// Initial Template for C++
 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+// C program for implementation of Bubble sort
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+// swapping the elements
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
 
 
 // } Driver Code Ends
-class Solution
-{
-    public:
-    void insert(int arr[], int i)
-    {
-        //code here
-        int j = i;
-    
-        while(j>0 && arr[j-1]>arr[j]){
-            int temp = arr[j-1];
-            arr[j-1]=arr[j];
-            arr[j]=temp;
-            j--;
-        }
-        
-    }
-     public:
-    //Function to sort the array using insertion sort algorithm.
-    void insertionSort(int arr[], int n)
-    {
-        //code here
-       
-        for(int i=0;i<=n-1;i++){
-            insert(arr,i);
+// User function Template for C++
+
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n) {
+        // Your code here
+        for(int i=n-1;i>=0;i--){
+            int swapped=0;
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapped = 1;
+                }
+            }
+            if(swapped==0){
+                break;
+            }
         }
     }
 };
 
+
 //{ Driver Code Starts.
-int main()
-{
-    int arr[1000],n,T,i;
 
-    scanf("%d",&T);
+/* Function to print an array */
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
-    while(T--){
+// Driver program to test above functions
+int main() {
+    int arr[1000], n, T, i;
 
-    scanf("%d",&n);
+    scanf("%d", &T);
 
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
+    while (T--) {
 
-    Solution ob;
-    ob.insertionSort(arr, n);
-    printArray(arr, n);
+        scanf("%d", &n);
+
+        for (i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+
+        Solution ob;
+
+        ob.bubbleSort(arr, n);
+        printArray(arr, n);
     }
     return 0;
 }
-
 // } Driver Code Ends
