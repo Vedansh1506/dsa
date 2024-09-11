5524a0ff6bf36238e9c86da3d69dbb28b51b62d2
Vedansh1506 <vedanshpatel1506@gmail.com>
2024-09-11 20:27:03 +0530

    quick sort

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,7 +1,7 @@
 //{ Driver Code Starts
-// C program for insertion sort
 #include <stdio.h>
-#include <math.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 /* Function to print an array */
 void printArray(int arr[], int size)
@@ -12,56 +12,60 @@ void printArray(int arr[], int size)
     printf("\n");
 }
 
-
-
 // } Driver Code Ends
 class Solution
 {
     public:
-    void insert(int arr[], int i)
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
     {
-        //code here
-        int j = i;
-    
-        while(j>0 && arr[j-1]>arr[j]){
-            int temp = arr[j-1];
-            arr[j-1]=arr[j];
-            arr[j]=temp;
-            j--;
+        // code here
+        if(low<high){
+            int pivot = partition(arr,low,high);
+            quickSort(arr,low,pivot-1);
+            quickSort(arr,pivot+1,high);
         }
         
     }
-     public:
-    //Function to sort the array using insertion sort algorithm.
-    void insertionSort(int arr[], int n)
+    
+    public:
+    int partition (int arr[], int low, int high)
     {
-        //code here
+       // Your code here
+       int pivot = arr[low];
+       int i = low;
+       int j = high;
        
-        for(int i=0;i<=n-1;i++){
-            insert(arr,i);
+       while(i<j){
+           while(arr[i]<=pivot && i<=high-1){
+               i++;
+           }
+           while(arr[j]>pivot && j>=low+1){
+               j--;
+           }
+           if(i<j){
+               swap(arr[i],arr[j]);
+           }
         }
+        swap(arr[low],arr[j]);
+        return j;
     }
 };
+
 
 //{ Driver Code Starts.
 int main()
 {
     int arr[1000],n,T,i;
-
     scanf("%d",&T);
-
     while(T--){
-
     scanf("%d",&n);
-
     for(i=0;i<n;i++)
       scanf("%d",&arr[i]);
-
-    Solution ob;
-    ob.insertionSort(arr, n);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
     printArray(arr, n);
     }
     return 0;
 }
-
 // } Driver Code Ends
