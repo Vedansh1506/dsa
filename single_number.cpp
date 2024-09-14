276608bfb11924b71fc4e7772d60343ce5f3996c
Vedansh1506 <vedanshpatel1506@gmail.com>
2024-09-15 00:54:48 +0530

    longest subarray with sum k (positives and  negatives)

--- single_number.cpp
+++ single_number.cpp
@@ -18,12 +18,13 @@ public:
 };
 
 // approach 2
-// class Solution {
-// public:
-//     int singleNumber(vector<int>& nums) { 
-//        int ans=0;
-// 	   for(auto x:nums)
-// 	   ans^=x;
-// 	   return ans;
-//     }
-// };
+// #include<vector>
+
+// int getSingleElement(vector<int> &arr){
+// 	// Write your code here.
+// 	int xorr = 0;
+// 	for(int i=0;i<arr.size();i++){
+// 		xorr = xorr ^ arr[i];
+// 	}
+// 	return xorr;	
+// }
