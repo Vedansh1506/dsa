ad09245878ab58c364663a92ce1a3f9739ccf71c
Vedansh1506 <vedanshpatel1506@gmail.com>
2024-09-15 02:38:24 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -250,3 +250,83 @@ void getStarPattern(int n) {
         cout<<endl;
     }
 }
+
+void symmetry(int n) {
+    // Write your code here.
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){     
+            cout<<"* ";
+        }
+        for(int j=1;j<=2*i-1;j++){    
+            cout<<" ";
+        }
+        for(int j=1;j<=n-i+1;j++) {       
+            cout<<"* ";
+        }
+        cout << endl;
+    }
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"* ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+void symmetry(int n) {
+    for(int i=n;i>0;i--){
+        for(int j=1;j<n-i+1;j++){
+            cout<<"* ";
+        }
+        for(int j=1;j<2*i-1;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<n-i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){     
+            cout<<"* ";
+        }
+        for(int j=1;j<=2*i-1;j++){    
+            cout<<" ";
+        }
+        for(int j=1;j<=n-i+1;j++) {       
+            cout<<"* ";
+        }
+        cout << endl;
+    }
+}
+
+void getNumberPattern(int n) {
+    for(int i=0;i<2*n-1;i++){
+         
+         // inner loop for no. of columns.
+         for(int j=0;j<2*n-1;j++){
+             
+             // Initialising the top, down, left and right indices of a cell.
+             int top = i;
+             int bottom = j;
+             int right = (2*n - 2) - j;
+             int left = (2*n - 2) - i;
+             
+             // Min of 4 directions and then we subtract from n
+             // because previously we would get a pattern whose border
+             // has 0's, but we want with border N's and then decreasing inside.
+             cout<<(n- min(min(top,bottom), min(left,right)));
+         }
+         
+         // As soon as the numbers for each iteration are printed, we move to the
+         // next row and give a line break otherwise all numbers
+         // would get printed in 1 line.
+         cout<<endl;
+     }
+}
