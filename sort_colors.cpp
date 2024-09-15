14cbe77f8a7aa3bbc39a47bd00932faa71b72d57
Vedansh1506 <vedanshpatel1506@gmail.com>
2024-09-15 17:02:31 +0530

    sort an array of 0's 1's and 2's

---
+++ sort_colors.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+            else mid++;
+        }
+    }
+};
+// for(int i=nums.size()-1;i>=0;i--){
+        //     int swapped=0;
+        //     for(int j=0;j<=i-1;j++){
+        //         if(nums[j]>nums[j+1]){
+        //             int temp = nums[j];
+        //             nums[j]=nums[j+1];
+        //             nums[j+1]=temp;
+        //             swapped=1;
+        //         }
+
+        //     }
+        //     if(swapped==0){
+        //         break;
+        //     }
+        // }
