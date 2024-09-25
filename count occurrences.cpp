b492470e4d805ffcc6d1a741f86d9ec6b5e89de2
Vedansh1506 <vedanshpatel1506@gmail.com>
2024-09-25 16:55:24 +0530

    Create count occurrences.cpp

---
+++ count occurrences.cpp
@@ -0,0 +1,126 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            first = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            last = mid;
+            //look for larger index on the right
+            low = mid + 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return last;
+}
+
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+int count(vector<int>& arr, int n, int x) {
+    pair<int, int> ans = firstAndLastPosition(arr, n, x);
+    if (ans.first == -1) return 0;
+    return (ans.second - ans.first + 1);
+}
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int low=0;
+	    int high=n-1;
+	    int first=-1;
+	    int last=-1;
+	    while(low<=high){
+	        int mid=low+(high-low/2);
+	        if(arr[mid]==x){
+	            first=mid;
+	            high=mid-1;
+	        }
+	        else if(arr[mid]<x) low=mid+1;
+	        else high=mid-1;
+	    }
+	    if(first==-1) return 0;
+	    
+	    low=0;
+	    high=n-1;
+	    while(low<=high){
+	        int mid=low+(high-low/2);
+	        if(arr[mid]==x){
+	            last=mid;
+	            low=mid+1;
+	        }
+	        else if(arr[mid]<x) low=mid+1;
+	        else high=mid-1;
+	    }
+	    return last-first+1;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
