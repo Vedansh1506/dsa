6e02ecd4558069d7fb77a90851183ece61beab58
Vedansh1506 <vedanshpatel1506@gmail.com>
2024-12-16 15:13:28 +0530

    Create count_digits.cpp

---
+++ basic maths/count_digits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        // int cnt = (int)(log10(n)+1);
+        int cnt = 0;
+        int num=N;
+        
+        while(N>0){
+            int ld = N%10;
+            if(ld!=0 && num%ld==0) cnt++;
+            N=N/10;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
